2bb2e2d419c558f884d744c41be070d25fc05fc4
Matt Warner <mattwarner@gmail.com>
2014-10-25 00:38:04 -0600

    makeCacheMatrix can now store and expose a matrix.  No inverse yet.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,22 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL           #initializes the cached inverse matrix
+    set <- function(y) {
+         x <<- y              #allows external calls to overwrite x in this environment         
+         inverse <<- NULL     #re-initialize the cached inverse matrix
+    }
+    get <- function() x
+    setInverse <- function(i) inverse <<- i #allows external calls to overwrite the cached matrix
+    getInverse <- function() inverse #returns the cached inverse matrix
+    
+    ## Generate a list containing the four function pointers that expose the
+    ## matrix and its inverse (if cached)
+    list(set = set, 
+         get = get, 
+         setInverse = setInverse,
+         getInverse = getInverse)
+  
 }
 
 
@@ -15,27 +30,27 @@ cacheSolve <- function(x, ...) {
 }
 
 makeVector <- function(x = numeric()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
-  list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
+     m <- NULL
+     set <- function(y) {
+          x <<- y
+          m <<- NULL
+     }
+     get <- function() x
+     setmean <- function(mean) m <<- mean
+     getmean <- function() m
+     list(set = set, get = get,
+          setmean = setmean,
+          getmean = getmean)
 }
 
 cachemean <- function(x, ...) {
-  m <- x$getmean()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- mean(data, ...)
-  x$setmean(m)
-  m
+     m <- x$getmean()
+     if(!is.null(m)) {
+          message("getting cached data")
+          return(m)
+     }
+     data <- x$get()
+     m <- mean(data, ...)
+     x$setmean(m)
+     m
 }
